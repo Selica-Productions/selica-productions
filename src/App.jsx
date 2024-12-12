dc8d4fbffcf9241f34b600508d629c83349d7285
Denise Sommer <jessicadenisesommer@gmail.com>
2024-12-12 20:08:54 +0100

    API funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/ui/navbar/Navbar";
+import FilmList from "./components/film-list/film-list";
 
 function App() {
   return (
     <>
       <Navbar />
+      <FilmList />
     </>
   );
 }
