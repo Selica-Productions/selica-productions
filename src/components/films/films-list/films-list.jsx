daf6de05eae97eae619a548e09e96c43660a0cbf
Selena Sánchez <sele71098@gmail.com>
2024-12-15 21:39:55 +0100

    refactor: restructure API calls into service files

--- src/components/films/films-list/films-list.jsx
+++ src/components/films/films-list/films-list.jsx
@@ -1,25 +1,24 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
-
-const API_URL = "https://api.themoviedb.org/3/movie/popular";
-const token = import.meta.env.VITE_MOVIE_TOKEN;
+import { getPopularMovies } from "../../../service/moviesService";
 
 function FilmsList(/*{ filter = {} }*/) {
   const [films, setFilms] = useState([]);
+  const [error, setError] = useState();
   // const [filteredFilms, setFilteredFilms] = useState([]);
 
-  const getAllFilms = () => {
-    axios
-      .get(`${API_URL}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => setFilms(response.data.results))
-      .catch((error) => console.log(error));
-  };
+  const getFilms = async() => {
+    try{
+      const popularMovies = await getPopularMovies();
+      setFilms(popularMovies);
+    } catch(e){
+      console.error(error);
+      setError(e.message)
+    }
+  }
 
   useEffect(() => {
-    getAllFilms();
+    getFilms();
   }, []);
 
   // useEffect(() => {
