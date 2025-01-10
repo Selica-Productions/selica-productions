f9ade2ee89bf23e3b03f5684e87b6f67073d95fb
Selena <sele71098@gmail.com>
2025-01-10 18:32:16 +0100

    style(searchbar): update input layout and styling

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -1,13 +1,15 @@
+import "./searchbar.css"
+
 function SearchBar({ search, onChange }) {
   return (
     <form className="d-flex gap-1 w-100 justify-content-center" role="search">
       <div className="d-flex align-items-center w-75 position-relative">
-        <i className="fa-solid fa-magnifying-glass position-absolute p-3"></i>
+        <i className="lens fa-solid fa-magnifying-glass position-absolute p-3"></i>
         <input
           id="search"
           type="text"
           placeholder="  Search by film title..."
-          className="form-control me-2 px-4"
+          className="searchbar"
           name="title"
           value={search}
           onChange={onChange}
